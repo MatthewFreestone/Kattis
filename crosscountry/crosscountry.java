3acb358625d34c99491185541fa81191cec2dc27
Matthew Freestone <matthewfreestone1593@gmail.com>
2020-10-14 17:14:35 -0500

    cross country initial work

--- crosscountry/crosscountry.java
+++ crosscountry/crosscountry.java
@@ -1,4 +1,41 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class crossCountry(){
+public class crosscountry{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int start = in.nextInt();
+        int end = in.nextInt(); 
+
+        intersection[] table = new intersection[N];
+        for(int i = 0; i < N; i++){
+            table[i] = new intersection(N); //gets a row
+            for (int j = 0; j < N; j++){
+                table[i].routeList[j] = in.nextInt();
+            }
+        }
+        ArrayList<Integer> sptList = new ArrayList<Integer>();
+        table[start].weightFromStart = 0;
+        for (int i = 0; i < N; i++){
+            table[i].weightFromStart = table[start].routeList[i];
+        }
+
+
+
+        
+
+    }
+}
+
+class intersection{
+    int weightFromStart = Integer.MAX_VALUE;
+    int index = 0; //what intersection this is
+    int[] routeList = null;
+    public intersection(int N){
+        routeList = new int[N];
+    }
+    
+
 
 }
