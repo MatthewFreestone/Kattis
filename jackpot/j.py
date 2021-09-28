63b69a5f6bb45f8e1132718a36ee59c17ab88239
Matthew Freestone <matthewfreestone1593@gmail.com>
2021-09-28 10:33:55 -0500

    finished jackpot

--- jackpot/j.py
+++ jackpot/j.py
@@ -1,2 +1,24 @@
+import math
+def main():
+	m = int(input())
+	for _ in range(m):
+		wheels = int(input())
+		if wheels == 1:
+			print(input())
+			continue
+
+		ins = map(int,input().split())
+		lcm_c = 1 
+		for i in ins:
+			lcm_c = lcm(lcm_c, i)
+			if lcm_c > 10**9:
+				print("More than a billion.")
+				break;
+		if lcm_c <= 10**9:
+			print(lcm_c)
+
 def lcm(a,b):
+	return abs(a*b) // math.gcd(a,b)
 	
+if __name__ == '__main__':
+	main()
