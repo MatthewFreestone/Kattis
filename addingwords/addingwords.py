87522bffbe88d57a119f9ac5925fb0b1ff473326
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-08-28 10:04:50 -0500

    finished addingwords, kinda befuddled

--- addingwords/addingwords.py
+++ addingwords/addingwords.py
@@ -1,43 +1,36 @@
 # Rating: ~ 4.2 / 10
 # Link: https://open.kattis.com/problems/addingwords
+
+# This problem was incredibly frustrating, I still don't know why the .bak version is wrong.
+# Notably, the problem thinks that "calc foo = " should return unknown NOT foo if foo is not defined. `
+
 import sys
 
 def main():
     w2i = {}
-    i2w = {}
     for line in sys.stdin:
         cmd, *tokens = line.strip().split()
         if cmd == 'def':
             w, i = tokens
-            i = int(i)
-            w2i[w] = i
-            i2w[i] = w
+            w2i[w] = int(i)
         elif cmd == 'calc':
             res_phrase = ' '.join(tokens)
-            startw, *tokens = tokens
-            #print(startw, w2i)
-            if startw not in w2i:
-                print(res_phrase, 'unknown')
-                continue
-            total = w2i[startw]
-            cmds = ((tokens[i], tokens[i+1]) for i in range(0, len(tokens)-1, 2))
-            # print(list(cmds))
-            unknown = False
-            for operator, w in cmds:
-                if w not in w2i:
-                    print(res_phrase, 'unknown')
-                    unknown = True
-                    break
-                if operator == '-':
-                    total -= w2i[w]
+
+            to_eval = []
+            for word in tokens[:-1]:
+                if word in ['+','-']:
+                    to_eval.append(word)
+                elif word in w2i:
+                    to_eval.append(str(w2i[word]))
                 else:
-                    total += w2i[w]
-            if not unknown:
-                #print(total, i2w)
-                res = i2w.get(total, 'unknown')
-                print(res_phrase, res)
+                    print(res_phrase, 'unknown')
+                    break
+            else:
+                res = eval(' '.join(to_eval))
+                i2w = {v:k for k,v in w2i.items()}
+                print(res_phrase, i2w.get(res, 'unknown'))
+
         elif cmd == 'clear':
             w2i = {}
-            i2w = {}
 if __name__ == "__main__":
   main()
