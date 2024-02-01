aef559b91136302694e0705ad56b13fd7ae7d7ff
Matthew Freestone <matthewfreestone1593@gmail.com>
2024-02-01 09:17:06 -0600

    finally. aojawhdajwjwa

--- creativeaccounting/creativeaccounting.cpp
+++ creativeaccounting/creativeaccounting.cpp
@@ -10,38 +10,39 @@ int main() {
         cin >> c;
         days.push_back(c);
     }
-    vector<int> prefix(n+1);
-    for (i = 1; i <= n; i++) {
-        prefix[i] = prefix[i-1] + days[i-1];
+    vector<int> prefix(n);
+    prefix[0] = days[0];
+    for (i = 1; i < n; i++) {
+        prefix[i] = prefix[i-1] + days[i];
         // cout << prefix[i] << " ";
     }
     auto rsq = [&](int s, int t) {
-        int l = (s > 0) ? prefix[s] : 0;
-        return prefix[t] - l;
+        if (s == 0) return prefix[t];
+        return prefix[t] - prefix[s-1];
     };
-    cout << rsq(0, 1) << endl;
-    cout << rsq(0, 3) << endl;
-    cout << rsq(1, 3) << endl;
     int lowest = 1e9;
     int highest = 0;
     for (int segment_size = l; segment_size <= h; segment_size++) {
         for (int offset = 0; offset < segment_size; offset++) {
             // check this solution
             int total = 0;
-            if (offset > 0) {
-                total += rsq(0, offset) > 0 ? 1 : 0;
-            }
-            for (i = offset; i < n; i += segment_size){
-                total += rsq(i, min(i+segment_size, n)) > 0 ? 1 : 0;
+            int i = offset;
+            while (true)
+            {
+                if (i == 0) {
+                    i += segment_size;
+                    continue;
+                }
+                int left = max(i - segment_size, 0);
+                int right = min(i, n);
+                total += rsq(left, right-1) > 0 ? 1 : 0;
+                if (i >= n) break;
+                i += segment_size;
             }
             highest = max(highest, total);
             lowest = min(lowest, total);
         }
     }
     cout << lowest << " "  << highest << endl;
-            
-
-
     return 0;
-
 }
